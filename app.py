54505683b70f9344e293d7ff1473b34a8a050957
mwr2 <mwr2@clemson.edu>
2025-12-05 20:15:39 -0500

    pic upload, send pic to ocr_space, get request

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        file = request.files["file"]  # get uploaded file
+        if file:
+            # send to OCR.space
+            url_api = "https://api.ocr.space/parse/image"
+            payload = {"apikey": "K81796487388957", "language": "eng"}
+            response = requests.post(url_api, data=payload, files={"file": file})
+            result = response.json()
+            return result["ParsedResults"][0]["ParsedText"]
     return render_template("form.html")
 
 if __name__ == "__main__":
