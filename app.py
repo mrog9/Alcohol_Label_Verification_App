f2bc3b5a51b76502d27792308eb5742f69581252
mwr2 <mwr2@clemson.edu>
2025-12-05 20:54:10 -0500

    trouble with file upload

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ def upload_file():
     if request.method == "POST":
         file = request.files["file"]  # get uploaded file
         if file:
-            # send to OCR.space
-            url_api = "https://api.ocr.space/parse/image"
-            payload = {"apikey": "K81796487388957", "language": "eng"}
-            response = requests.post(url_api, data=payload, files={"file": file})
-            result = response.json()
-            return result
+
+            # url_api = "https://api.ocr.space/parse/image"
+            # payload = {"apikey": "K81796487388957", "language": "eng"}
+            # response = requests.post(url_api, data=payload, files={"file": file})
+            # result = response.json()
+            return str(file)
     return render_template("form.html")
 
 if __name__ == "__main__":
