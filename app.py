702ab2c68c3006071a69b1463b9a5da6b14cb13d
mwr2 <mwr2@clemson.edu>
2025-12-06 20:20:49 -0500

    trying to use docker build

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from utils import *
+import os
 
 app = Flask(__name__)
 
@@ -24,7 +25,8 @@ def upload_file():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
 
 
 
